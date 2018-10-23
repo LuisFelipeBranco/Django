2970d155f7a059085f2c12cb21ead2ce44d6d449
LuisFelipe <bpl_@outlook.com>
2018-10-23 16:34:02 -0300

    Criação de urls genéricas em music para futuras configurações.

--- website/music/views.py
+++ website/music/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Music app created!!</h1>")
+
+def detail(request, album_id):
+    return HttpResponse("<h2>Details for Album ID: " + str(album_id) + "</h2>")
